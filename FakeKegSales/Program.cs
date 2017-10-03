b86f774ed83d54260164747e0b84e6feff4d38aa
Barry <bmarons1@gmail.com>
2017-10-02 21:32:24 -0500

    Outline of KegSale Class

--- FakeKegSales/Program.cs
+++ FakeKegSales/Program.cs
@@ -2,9 +2,19 @@
 using System.Dynamic;
 
 namespace FakeKegSales
-{
-    
+{ 
 
+    public class KegSale
+    {
+        public string KegType { get; set; }
+        public string Distributor { get; set; }
+        public DateTime SaleTime { get; set; }
+
+        public override string ToString()
+        {
+            return KegType + "," + Distributor + " " + SaleTime;
+        }
+    }
 
     class Program
     {
