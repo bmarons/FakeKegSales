ea69a4f535c1b67c0b0cbede090e4bf83b35472e
Barry <bmarons1@gmail.com>
2017-10-03 01:58:57 -0500

    implamrnted GetDistributor as a Dictionary and compared speed

--- FakeKegSales/FakeKegSales.cs
+++ FakeKegSales/FakeKegSales.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace FakeKegSales
 { 
@@ -24,13 +26,74 @@ namespace FakeKegSales
 
     class FakeKegSales
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("I am the main program");
+
+            List<string> beersList = new List<string>()
+            {
+                "1/2 Miller Lite",
+                "1/4 Miller Lite",
+                "1/2 Blue Moon",
+                "1/6 Blue Moon",
+                "1/2 Bud Light",
+                "1/4 Bud Light",
+                "1/2 Budweiser",
+                "1/6 Budweiser",
+                "1/2 Coors Light",
+                "1/4 Coors Light",
+                "1/2 Dos Equis Amber",
+                "1/2 Dos Equis Lager",
+                "1/6 Dos Equis Lager",
+                "1/2 Michelob Ultra",
+                "1/6 Michelob Ultra",
+                "1/2 Natural Light",
+                "PBR 1/2 Barrel Keg",
+                "1/2 Yuengling Lager",
+                "1/4 Yuengling Lager",
+
+            };
+
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int r = rnd.Next(beersList.Count);
+                Console.WriteLine(HelperFunctions.GetDistributorDict(beersList[r]));
+
+            }
+
+            stopwatch2.Stop();
+
+
+            Stopwatch stopwatch = new Stopwatch();
+
+
+
+            stopwatch.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int r = rnd.Next(beersList.Count);
+                Console.WriteLine(HelperFunctions.GetDistributor(beersList[r]));
+                
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Time elapsed: {0}", stopwatch2.Elapsed);
+
+
+
+
         }
 
-        
 
     }
+
+       
 
 }
--- FakeKegSales/HelperFunctions.cs
+++ FakeKegSales/HelperFunctions.cs
@@ -42,13 +42,45 @@ namespace FakeKegSales
 
         };
 
+        public static Dictionary<string, string> BeerToDDistributorDict = new Dictionary<string, string>()
+        {
+            { "1/2 Blue Moon","Miller of the Rockies"},
+            { "1/6 Blue Moon","Miller of the Rockies"},
+            { "1/2 Bud Light","Eagle Dist Co"},
+            { "1/4 Bud Light","Eagle Dist Co"},
+            {"1/2 Budweiser","Eagle Dist Co"},
+            {"1/6 Budweiser","Eagle Dist Co"},
+            {"1/2 Coors Light","Miller of the Rockies"},
+            {"1/4 Coors Light","Miller of the Rockies"},
+            {"1/2 Dos Equis Amber","Miller of the Rockies"},
+            {"1/2 Dos Equis Lager","Miller of the Rockies"},
+            {"1/6 Dos Equis Lager","Miller of the Rockies"},
+            {"1/2 Michelob Ultra","Eagle Dist Co"},
+            {"1/6 Michelob Ultra","Eagle Dist Co"},
+            {"1/2 Miller Lite","Miller of the Rockies"},
+            {"1/4 Miller Lite","Miller of the Rockies"},
+            {"1/2 Natural Light","Eagle Dist Co"},
+            {"PBR 1/2 Barrel Keg","Miller of the Rockies"},
+            {"1/2 Yuengling Lager","PA Proud"},
+            {"1/4 Yuengling Lager","PA Proud"},
+
+        };
+
         public static string GetDistributor(string beerName)
         {
-
             var distributor = BeerToDDistributor.First(kvp => kvp.Key == beerName).Value;
             return distributor;
         }
 
+
+        public static string GetDistributorDict(string beerName)
+        {
+
+            
+
+            var distributor = BeerToDDistributorDict[beerName];
+            return distributor;
+        }
 
         private static List<string> _beersList = new List<string>()
         {
